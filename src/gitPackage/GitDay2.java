a8db1339bcfc5271b751d88ca7f29f277b85591a
Mahmut Yigit <mhmtygt.3090@gmail.com>
2020-03-16 01:38:12 +0200

    our fifth commit

--- src/gitPackage/GitDay2.java
+++ src/gitPackage/GitDay2.java
@@ -3,9 +3,9 @@ package gitPackage;
 public class GitDay2 {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello world!");
-		
-		
+		System.out.println("Hello Git Users!");
 	}
 
 }
